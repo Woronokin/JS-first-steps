30f736b7652261d0e05b4b464db326ebf0fef046
Philipp <woronokin@icloud.com>
2020-08-16 15:56:11 +0300

    комментарии и переменные

--- hello.js
+++ hello.js
@@ -1,2 +1,7 @@
 console.log('Hello!');
+// добавлена скрытая строка
+/* скрытие для бодьших текстов или кусков кода */
 console.log('This is my first steps in JavaScript');
+var name; //объявление переменной
+name = 'Woronokin'; //присваивание 
+console.log(name); //вывод переменной
